0f9667e4c2a25b2310680fc5fbb58408cbe18662
AndrewSigorskih <carolusrex8547@gmail.com>
2022-08-22 10:52:12 +0300

    added bugged behaviour

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -6,3 +6,6 @@ else
     echo -e "$dir is not a directory!\n"
     exit 101
 fi
+
+top1=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | cut -f 2)
+disk=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | cut -f 1)
